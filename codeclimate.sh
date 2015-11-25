39c2f6096e368065582d81d1e528c1c68bf4d5ce
Floyd Pink <imbleedingme@googlemail.com>
2015-11-25 16:41:43 -0500

    push coverage to codeclimate

--- codeclimate.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-npm install -g istanbul
-istanbul cover node_modules/grunt-contrib-nodeunit/node_modules/nodeunit/bin/nodeunit -- test
-npm install -g codeclimate-test-reporter
-codeclimate-test-reporter < coverage/lcov.info
